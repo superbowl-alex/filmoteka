03fae0cd30514044482342a3799f4efc5d1f4d3d
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-09-11 20:53:00 +0300

    project structure improved

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 import ApiQuery from './js_components/api_query';
 import { renderTrendMovies } from './js_components/trendMoviesCards';
-import { onSearchMovie } from './searchMovie';
+import { onSearchMovie } from './js_components/searchMovie';
 
 renderTrendMovies();
 const form = document.querySelector('.form-header');
