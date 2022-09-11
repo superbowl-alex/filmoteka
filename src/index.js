76f31dca5daee8ff967a27f2e28ba2dcf2beba07
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-09-11 13:35:20 +0300

    new js file for library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 import ApiQuery from './js_components/api_query';
-import headerHeight from "./js-components/header-height";
 import { renderTrendMovies } from './trendMoviesCards';
 renderTrendMovies();
--- src/js-components/header-height.js
+++
@@ -1,7 +0,0 @@
-// (() => {
-//     const { height: headerHeight } = document
-//         .querySelector(".header-homepage")
-//         .getBoundingClientRect();
-
-//     document.body.style.paddingTop = `${headerHeight}px`;
-// })();
