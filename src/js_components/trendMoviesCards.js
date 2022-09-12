a7f47936af75737c2a241a49a6113bd40e4fcbca
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-09-12 16:53:25 +0300

    modal info added

--- src/js_components/trendMoviesCards.js
+++ src/js_components/trendMoviesCards.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import Notiflix from 'notiflix';
 import 'notiflix/dist/notiflix-3.2.5.min.css';
-import { genres } from '../Genres/genres.json';
+import { onModalOpen } from './close-modal';
+import { createElementFromHTML, getGenres } from './helpers';
 import { addLoadMoreBtn, removeLoadMoreBtn } from './load-more-button';
 import { loadMoreMovies } from './searchMovie'
 
@@ -23,7 +24,10 @@ export async function renderTrendMovies(page) {
     const response = await fetchTrendingMovies(page);
     const movies = await response.results;
     totalPages = await response.total_pages;
-    gallery.insertAdjacentHTML('beforeend', movieCard(movies));
+
+    const elements = getMovieElements(movies);
+    elements.forEach(element => gallery.insertAdjacentElement('beforeend', element));
+
     if (totalPages === page) {
       return Notiflix.Notify.failure(`This is the last page`, {
         width: '400px',
@@ -55,23 +59,13 @@ loadMoreBtn.addEventListener('click', () => {
   loadMore(renderTrendMovies, currentPage);
 });
 
-export function movieCard(movies) {
+export function getMovieElements(movies) {
   return movies
-    .map(({ original_title, release_date, id, poster_path, genre_ids }) => {
-      const genresArray = [];
-      let movieGenres = '';
+    .map((movie) => {
+      const { original_title, release_date, id, poster_path, genre_ids } = movie
+      let movieGenres = getGenres(genre_ids, true);
       const movieRelease = new Date(release_date).getFullYear();
 
-      genre_ids.map(id => {
-        return genres.find(el => {
-          if (el.id === id) return genresArray.push(el.name);
-        });
-      });
-      if (genresArray.length > 2) {
-        movieGenres = `${genresArray[0]}, ${genresArray[1]}, Other`;
-      } else {
-        movieGenres = `${genresArray.join(', ')}`;
-      }
       const image = poster_path
         ? `<div class="gallery-item__image-wrap">
               <picture>
@@ -91,13 +85,16 @@ export function movieCard(movies) {
                 </svg>     
            </div>`;
 
-      return `
+      const element = createElementFromHTML(`
         <li class='gallery-item'>
             ${image}
             <p class="gallery-item__title">${`${original_title.toUpperCase()}`}</p>
             <p class="gallery-item__info">${`${movieGenres} | ${movieRelease}`}</p>
         </li>
-          `;
-    })
-    .join('');
+      `);
+
+      element.addEventListener('click', e => onModalOpen(e, movie));
+
+      return element;
+    });
 }
