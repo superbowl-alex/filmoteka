9a43357afe6115be9e1f59e59d6b3122a151a1dd
Danyil-Repenko <danyil.oficial@gmail.com>
2022-09-12 21:12:36 +0300

    додав плавний скролл

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const axios = require('axios');
 import ApiQuery from './js_components/api_query';
-
+import OnlyScroll from 'only-scrollbar';
 import { renderTrendMovies } from './js_components/trendMoviesCards';
 import { onSearchMovie } from './js_components/searchMovie';
 import getRefs from './js_components/getRefs';
 const refs = getRefs();
+const scroll = new OnlyScroll(document.scrollingElement, {
+    damping: 0.8,
+});
 
 renderTrendMovies();
 refs.form.addEventListener('submit', onSearchMovie);
