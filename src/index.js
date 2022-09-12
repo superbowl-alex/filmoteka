fe1a94e4cbf995e2ad799c67ecef254fc2daf97e
Oleksandr <superbowl@ukr.net>
2022-09-12 17:58:20 +0300

    Add warning about last page

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ApiQuery from './js_components/api_query';
 
 import { renderTrendMovies } from './js_components/trendMoviesCards';
 import { onSearchMovie } from './js_components/searchMovie';
+import getRefs from './js_components/getRefs';
+const refs = getRefs();
 
 renderTrendMovies();
-const form = document.querySelector('.form-header');
-form.addEventListener('submit', onSearchMovie);
+refs.form.addEventListener('submit', onSearchMovie);
 import './js_components/close-modal';
