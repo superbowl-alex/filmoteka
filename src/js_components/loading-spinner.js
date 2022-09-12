4d26889cd0a4209854d79e97366e239bf7e36f02
DenisVoron <Denis.voronko@gmail.com>
2022-09-12 21:01:09 +0300

    add componets loading

---
+++ src/js_components/loading-spinner.js
@@ -0,0 +1,21 @@
+import { Loading } from 'notiflix/build/notiflix-loading-aio';
+
+console.log(Loading);
+
+export default function loading() {
+
+    const options = {
+        svgColor: '#ff001b',
+        svgSize: '120px',
+    }
+
+    Loading.dots(options);
+
+    removeLoading();
+}
+
+function removeLoading() {
+    setTimeout(Loading.remove, 500);
+}
+
+loading();
