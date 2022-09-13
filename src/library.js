c16e87e586d1a3d0fc3ab66d1cf72f4344d90110
Danyil-Repenko <danyil.oficial@gmail.com>
2022-09-13 20:07:19 +0300

    loadMore на watched

--- src/library.js
+++ src/library.js
@@ -1,6 +1,7 @@
 import OnlyScroll from 'only-scrollbar';
 import './js_components/loadWatchedMovies';
 import './js_components/openModalOnGallery';
+import './js_components/scroll-up';
 const scroll = new OnlyScroll(document.scrollingElement, {
     damping: 0.8,
 });
