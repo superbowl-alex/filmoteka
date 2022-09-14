99238a664690110443ff02aba09321bac09e3106
Yana Karpliuk <yanakarpliuk99@gmail.com>
2022-09-14 18:26:09 +0300

    modal empty poster fixed

--- src/js_components/openModalOnGallery.js
+++ src/js_components/openModalOnGallery.js
@@ -1,13 +1,51 @@
-import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/dist/basicLightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
 import { getGenres } from './helpers';
+import { scroll } from  '../library';
+
 const refs = {
   gallery: document.querySelector('.gallery'),
   template: document.querySelector('template'),
 };
 
-const lightbox = basicLightbox.create(refs.template);
-const renderModal = (template, movie) => {
+refs.gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(e) {
+  const item = e.target.closest('.js-item');
+  if (!item) return;
+  const data = JSON.parse(item.dataset.movie);
+
+  const lightbox = basicLightbox.create(refs.template, {
+    onShow: () => {
+      document.body.style.overflow = 'hidden';
+      scroll.lock();
+    },
+    onClose: () => {
+      document.body.style.overflow = 'visible';
+      scroll.unlock();
+    },
+  });
+
+  renderModal(lightbox.element(), data);
+  lightbox.show();
+
+  window.addEventListener('keydown', closeModalByEsc);
+  function closeModalByEsc(e) {
+    if (e.code === 'Escape') {
+      lightbox.close();
+      window.removeEventListener('keydown', closeModalByEsc);
+    }
+  }
+
+  const btnClose = document.querySelector('.modalfilm-button-close');
+  btnClose.addEventListener('click', closeModal);
+  function closeModal() {
+    lightbox.close();
+    btnClose.removeEventListener('click', closeModal);
+  }
+};
+
+function renderModal(template, movie) {
   template.querySelector('.movie-title').innerHTML = movie.title;
   template.querySelector('.about-text').innerHTML = movie.overview;
   template.querySelector('.vote').innerHTML = movie.vote_average.toFixed(1);
@@ -18,14 +56,7 @@ const renderModal = (template, movie) => {
     movie.genre_ids
   );
   template.querySelector("div.btn-l").style.display = "none";
-  const btnClose = document.querySelector('.modalfilm-button-close');
-
-  btnClose.addEventListener('click', closeModal);
-  function closeModal() {
-    lightbox.close();
-    btnClose.removeEventListener('click', closeModal);
-  }
-
+  
   if (movie.poster_path) {
     const modal = template.querySelector('.modalfoto-img');
     modal.src = `https://www.themoviedb.org/t/p/w780${movie.poster_path}`;
@@ -37,22 +68,3 @@ const renderModal = (template, movie) => {
     placeholder.style.display = 'flex';
   }
 };
-const onGalleryClick = e => {
-  const item = e.target.closest('.js-item');
-  if (!item) return;
-  const data = JSON.parse(item.dataset.movie);
-  lightbox.show();
-  renderModal(lightbox.element(), data);
-};
-
-const onBtnAddToWatchedClick = e => {};
-
-function onBtnClick(e) {
-  // console.log(e.target.className);
-  if (e.target.className === 'btn watched') {
-    onBtnAddToWatchedClick();
-  }
-}
-
-refs.gallery.addEventListener('click', onGalleryClick);
-document.addEventListener('click', onBtnClick);
