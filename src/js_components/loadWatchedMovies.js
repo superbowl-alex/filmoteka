b9606e6c454dfd419ad98dc7a2469b11b19ac3bc
Danyil-Repenko <danyil.oficial@gmail.com>
2022-09-14 07:33:56 +0300

    2

--- src/js_components/loadWatchedMovies.js
+++ src/js_components/loadWatchedMovies.js
@@ -1,6 +1,7 @@
 import { getMovieCard } from './getMovieCard';
 import { addLoadMoreBtn, removeLoadMoreBtn } from './load-more-button';
 import loading from '../js_components/loading-spinner';
+import { loadMoreQueueMovies } from './loadQueueMovies';
 
 const refs = {
   watchedBtn: document.querySelector('.js-watched-list'),
@@ -9,12 +10,10 @@ const refs = {
 };
 
 const KEY = 'watched-movies';
-//вивів markup та data oкремо, щоб в першій перевірці менше коду писати
 let markup = null;
 let data = null;
-//змінні, що потрібні для loadMore
 let totalPages = null;
-let page = 1;
+let page = null;
 
 const changeActiveBtn = () => {
   refs.queueBtn.classList.remove('active');
@@ -25,13 +24,12 @@ const onWatchedBtnClick = e => {
   changeActiveBtn();
   refs.gallery.innerHTML = '';
   data = JSON.parse(localStorage.getItem(KEY)) || [];
-  //записую загальну кількість сторінок; нічого, що число не ціле, так краще
   totalPages = data.length / 20;
+  page = 1;
   if (data.length === 0) {
   }
   markup = data.map(getMovieCard);
-  removeLoadMoreBtn();
-  //перша перевірка - якщо фільмів більше, ніж 20, завантажує лише перші 20 штук
+  removeLoadMoreBtn(loadMoreQueueMovies);
   if (totalPages > 1) {
     addLoadMoreBtn();
     const loadMoreBtn = document.querySelector('.load-more-button');
@@ -45,24 +43,13 @@ refs.watchedBtn.addEventListener('click', onWatchedBtnClick);
 
 onWatchedBtnClick();
 
-//На кожній сторінці і на кожному типі списку (в index це "popular" та "on search";
-//в library - "watched" та "queue") я просто щоразу домальовую нову кнопку та вішаю на неї потрібну функцію.
-//Оскільки на одній сторінці списки можуть чергуватися, треба перевіряти та видаляти
-//кнопку, що стосувалася попереднього (наприклад, після пошуку за словом кнопка зі списку "popular" вже не треба).
-//У дужки треба вписати функцію, що пропишеться в addEventListener у списку "queue".
-//Якщо нічого не вказати, кнопка усеодно видалиться, але слухач подій - ні.
-const loadMoreBtn = document.querySelector('.load-more-button');
-// loadMoreBtn.addEventListener('click', loadMoreWatchedMovies);
-function loadMoreWatchedMovies() {
+export function loadMoreWatchedMovies() {
   page += 1;
-  //різні сторінки - різні фільми
   const markup = data.filter((film, index) => index >= (page * 20 - 20) && index <= (page * 20 - 1)).map(getMovieCard);
   loading();
   refs.gallery.append(...markup);
   if (page >= totalPages) {
-    //ось - видаляємо і кнопку і слухача, коли сторінки закінчилися
     removeLoadMoreBtn(loadMoreWatchedMovies)
     return;
   }
 }
-//коли додасться список "queue", мені треба буде зробити те ж саме там, що і тут
